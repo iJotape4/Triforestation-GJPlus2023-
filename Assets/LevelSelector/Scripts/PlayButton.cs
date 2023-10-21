db054e9d826c8506e419c1aeb33e612a0cf8abfe
JotaPe <jcamacho249@unab.edu.co>
2023-10-21 16:01:12 -0500

    [Add] LevelSelector

---
+++ Assets/LevelSelector/Scripts/PlayButton.cs
@@ -0,0 +1,45 @@
+using MyBox;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace LevelSelector
+{
+    public class PlayButton : MonoBehaviour
+    {
+        [SerializeField, Scene] string sceneName;
+        [SerializeField] PregamePopUp pregamePopUp;
+        [SerializeField] public Button playButton;
+
+        private void Start()
+        {
+            pregamePopUp = GetComponentInParent<PregamePopUp>();
+            playButton =    GetComponent<Button>();
+
+            playButton.onClick.AddListener( ClickButton);
+            pregamePopUp.enablePlayButton += SwitchEnablePlayButton;
+        }
+        
+        private void SwitchEnablePlayButton(bool enable)=>
+            playButton.enabled= enable;
+
+        private void ClickButton()
+        {
+            SceneManager.LoadScene(sceneName);
+            //SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        //private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        //{
+        //    GameObject[] gameObjects = scene.GetRootGameObjects();
+        //    foreach (GameObject gameObject in gameObjects)
+        //    {
+        //        if (gameObject.GetComponent<LevelInstantiator>() != null)
+        //        {
+        //            gameObject.GetComponent<LevelInstantiator>().SetLevelData(pregamePopUp.level);
+        //            break;
+        //        }
+        //    }
+        //}
+    }
+}
