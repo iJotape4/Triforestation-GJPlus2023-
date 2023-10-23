27db57e105fa001e9eb657f948e6384183ae5542
Julian Gaitan <58521037+GaitanJulian@users.noreply.github.com>
2023-10-22 20:17:54 -0500

    Add GameManager

---
+++ Assets/Terraforming/Game Manager/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : SinglentonParent<GameManager>
+{
+
+    public Dictionary<ENUM_Biome, int> biomeCounts = new Dictionary<ENUM_Biome, int>();
+
+    public void SetDictionary(Dictionary<ENUM_Biome, int> dict)
+    {
+        biomeCounts = dict;
+    }
+}
