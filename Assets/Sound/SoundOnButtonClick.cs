8495d33214f06b5a70aa1c512ec65f4d9f5668fe
JotaPe <jcamacho249@unab.edu.co>
2024-01-13 11:52:15 -0500

    [Add] settings emnu placeholder

---
+++ Assets/Sound/SoundOnButtonClick.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Kitchen
+{
+    [RequireComponent(typeof(Button))]
+    public class SoundOnButtonClick : MonoBehaviour
+    {
+        [SerializeField] private string sound = "event:/SFX/UI/Click button";
+        private Button button;
+        private FMOD.Studio.EventInstance buttonSound;
+
+        private void Awake()
+        {
+            button = GetComponent<Button>();
+            button.onClick.AddListener(PlayButtonClickSound);
+            buttonSound = FMODUnity.RuntimeManager.CreateInstance(sound);
+        }
+
+        private void OnDestroy() =>
+            button.onClick.RemoveListener(PlayButtonClickSound);
+
+        public void PlayButtonClickSound() =>
+            buttonSound.start();
+    }
+}
