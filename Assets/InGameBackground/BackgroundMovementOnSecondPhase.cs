05ccda0d948376ba35a3a52d2104ad65902f7b74
JotaPe <jcamacho249@unab.edu.co>
2024-01-14 12:03:50 -0500

    [Fix] blocker on second level

--- Assets/InGameBackground/BackgroundMovementOnSecondPhase.cs
+++ Assets/InGameBackground/BackgroundMovementOnSecondPhase.cs
@@ -9,6 +9,10 @@ public class BackgroundMovementOnSecondPhase : MonoBehaviour
         EventManager.AddListener(ENUM_GameState.firstPhaseFinished, MoveArregement);
     }
 
+    private void OnDestroy()
+    {
+        EventManager.RemoveListener(ENUM_GameState.firstPhaseFinished, MoveArregement);
+    }
     private void MoveArregement()
     {
         transform.DOMoveY(-2, 1f);
