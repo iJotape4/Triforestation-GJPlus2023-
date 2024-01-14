16ba86d41fbcde1bc2d20c495d54e22ad7500487
JotaPe <jcamacho249@unab.edu.co>
2024-01-14 18:14:56 -0500

    [Add] UI text score when dropped animal

--- Assets/Terraforming/FoodChain/Scripts/AnimalBehaviour.cs
+++ Assets/Terraforming/FoodChain/Scripts/AnimalBehaviour.cs
@@ -1,12 +1,34 @@
 using Events;
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class AnimalBehaviour : MonoBehaviour
 {
     [SerializeField] public Animal animalData;
-
+    [SerializeField] TextMeshProUGUI animalText;
     public void OnAnimalDroped()
     {
        EventManager.Dispatch(ENUM_SFXEvent.animalSound,animalData.animalSFX);
+       animalText.gameObject.SetActive(true);
+        StartCoroutine(DisableTMP());
+    }
+
+    private void Start()
+    {
+        string text = animalData.chainLevel  switch
+        {
+            ENUM_FoodChainLevel.AnimalKing => "+3",
+            ENUM_FoodChainLevel.Predator => "+2",
+            ENUM_FoodChainLevel.Prey => "+1",
+            _=> "+1" 
+        };
+        animalText.text = text;
+    }
+
+    public IEnumerator DisableTMP()
+    {
+        yield return new WaitForSeconds(1f);
+        animalText.gameObject.SetActive(false);
     }
 }
